d5e9664ec50066794103acc9062969545c652fc9
Natalia Kim <natasha_kim5@yahoo.com>
2022-12-02 00:42:49 +0300

    .

---
+++ main-page/main.js
@@ -0,0 +1,8 @@
+// WELCOME-SECTION
+let menuBtn = document.querySelector('.menu-btn');
+let options = document.querySelector('.options');
+/* show/hide menu options when clicking on the menu button */
+menuBtn.addEventListener("click", () => {
+    options.classList.toggle("show-options");
+});
+// PROJECT-SECTION
--- main/main.js
+++
@@ -1,4 +0,0 @@
-let projectDescription = document.querySelector(".project-description");
-let slide = document.querySelector(".slide");
-// PROJECT-SECTION
-// slider
