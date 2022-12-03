c8f38ccf2376f9e5c59012192fe4363d17f89005
Natalia Kim <natasha_kim5@yahoo.com>
2022-12-03 23:28:03 +0300

    completed the menu

--- main-page/main.js
+++ main-page/main.js
@@ -1,8 +1,19 @@
 // WELCOME-SECTION
+let menu = document.querySelector(".menu");
 let menuBtn = document.querySelector('.menu-btn');
 let options = document.querySelector('.options');
 /* show/hide menu options when clicking on the menu button */
 menuBtn.addEventListener("click", () => {
     options.classList.toggle("show-options");
+    menu.classList.toggle("menu-background");
+});
+// show the menu background color when scrolling
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > menu.offsetHeight) {
+        menu.classList.add("menuScrolled");
+    }
+    else {
+        menu.classList.remove("menuScrolled");
+    }
 });
 // PROJECT-SECTION
